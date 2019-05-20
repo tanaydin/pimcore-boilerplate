80a51898a490735db528e2a3ecbe9548c80dba17
m.vanhoof <m.vanhoof@youwe.nl>
2019-05-20 11:46:06 +0200

    add composer update

--- update-pimcore-version.sh
+++ update-pimcore-version.sh
@@ -9,3 +9,5 @@ cp composer.json .composer-base.json
 line=$(grep \"pimcore/pimcore\":\ \" composer.json)
 version=$(echo $line | cut -d '"' -f 4)
 sed "s/$version/$1/g" .composer-base.json > composer.json
+
+docker-compose exec php bash -c "COMPOSER_MEMORY_LIMIT=-1 composer update --no-suggest --no-interaction --dev -vvv"
