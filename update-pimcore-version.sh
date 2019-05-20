facb3f71e75fcdab34cf69475c55d0d0e7d625cf
m.vanhoof <m.vanhoof@youwe.nl>
2019-05-20 11:01:24 +0200

    first version

--- update-pimcore-version.sh
+++ update-pimcore-version.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-line=$(grep \"pimcore/pimcore\":\ \" composer.json)
 
+if [ "$#" -ne 1 ]; then
+    echo "Please add new version number as a command-line argument"
+fi
+
+cp composer.json .composer-base.json
+
+line=$(grep \"pimcore/pimcore\":\ \" composer.json)
+version=$(echo $line | cut -d '"' -f 4)
+sed "s/$version/$1/g" .composer-base.json > composer.json
